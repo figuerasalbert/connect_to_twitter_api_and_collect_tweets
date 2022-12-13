5d720f71774136f45093ea11cd6b84e471e579a1
figuerasalbert <a.figueras5@gmail.com>
2022-12-13 23:28:52 +0100

    solve error

--- dags/python_scripts/twitter_api.py
+++ dags/python_scripts/twitter_api.py
@@ -63,6 +63,7 @@ twitter_df = pd.DataFrame(data_tweets, columns = columns)
 
 # Changing the format of column Date, eliminating the time zone
 twitter_df['Tweet_Date'] = pd.to_datetime(twitter_df.Tweet_Date).dt.tz_localize(None)
+
 print(str(len(twitter_df.index)) + ' Tweets successfully obtained')
 
 
